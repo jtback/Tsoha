da4bfa373e5f47262b5d031d0b04239bc5683cc7
Jukka A T Back <jtback@svm-19.cs.helsinki.fi>
2014-02-17 00:37:41 +0200

    html-demosivujen runkoja

--- html-demo/index.php
+++ html-demo/index.php
@@ -5,5 +5,13 @@
 <head><title><?php echo $tervehdys; ?></title></head>
 <body>
   <h1><?php echo $tervehdys; ?></h1>
+  <div>
+    <ul>
+        <li> <a href="../../Tsoha/html-demo/isannoitsijan_etusivu.htm">Isännöitsijän päänäkymään</a> </li>
+        <li> <a href="../../Tsoha/html-demo/asukkaan_etusivu.htm">Asukkaan päänäkymään</a> </li>
+        <li> <a href="../../Tsoha/html-demo/kirjautumisnakyma.htm">Kirjautumisnäkymään</a> </li>         
+    </ul>
+  </div>
+  
 </body>
 </html>
